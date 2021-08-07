26be75a6c98a62eaadb8e5d4cee04cfa1362ef9b
mikedada23 <75315814+mikedada23@users.noreply.github.com>
2021-08-07 10:26:34 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'd0cb77606b334171bb4b71ecf95c9ab3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Bien Maksim Blog',
+  domain: 'https://wandering-ton-cfe.notion.site/Bien-Maksim-Blog-d0cb77606b334171bb4b71ecf95c9ab3',
+  author: 'Mike Zhao',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  WeChat: 'lm0202zxs',
+  Github: 'mikedada23',
+  ZhiHu: '不考出证书不换名',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
